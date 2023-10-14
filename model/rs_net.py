a81166b99343bd917a11c5def44840e0f7736aa3
prajapatisarvesh <sarvesh101p@gmail.com>
2023-10-14 19:50:59 -0400

    added-rsnet

--- model/rs_net.py
+++ model/rs_net.py
@@ -31,8 +31,9 @@ class RSNet(BaseModel):
         self.vgg16_pretrained.features[4] = nn.MaxPool2d(kernel_size=2, stride=1, padding=0, dilation=1, ceil_mode=False)
         self.vgg16_pretrained.features[16] = nn.MaxPool2d(kernel_size=2, stride=1, padding=0, dilation=1, ceil_mode=False)
         self.vgg16_pretrained.features[30] = nn.MaxPool2d(kernel_size=2, stride=1, padding=0, dilation=1, ceil_mode=False)
+
         '''
-        Remove the classifier layer and avgpool layer
+        Remove the classifier layer and avgpool layeroutput_padding=1
         '''
         self.vgg16_pretrained = nn.Sequential(*list(self.vgg16_pretrained.children())[:-2])
         '''
@@ -50,12 +51,37 @@ class RSNet(BaseModel):
         '''
         self.prelu = nn.PReLU()
         self.decoder_conv1 = nn.Conv2d(512, 512, kernel_size=(3,3), stride=(1,1), padding=(1,1))
-        self.decoder_convt_1 = nn.ConvTranspose2d(512, 512, kernel_size=(3,3), stride=(1,1), padding=(0,0), dilation=(3,3))
-
+        self.decoder_convt_1 = nn.ConvTranspose2d(512, 512, kernel_size=(2,2), stride=(1,1), padding=(0,0))
+        self.decoder_conv2 = nn.Conv2d(512, 256, kernel_size=(3,3), stride=(1,1), padding=(1,1))
+        self.decoder_convt_2 = nn.ConvTranspose2d(256, 256, kernel_size=(2,2), stride=(2,2), padding=(0,0))
+        self.decoder_conv3 = nn.Conv2d(256, 256, kernel_size=(3,3), stride=(1,1), padding=(1,1))
+        self.decoder_conv4 = nn.Conv2d(256, 128, kernel_size=(3,3), stride=(1,1), padding=(1,1))
+        self.decoder_convt_3 = nn.ConvTranspose2d(128, 128, kernel_size=(2,2), stride=(1,1), padding=(0,0))
+        self.decoder_conv5 = nn.Conv2d(128, 128, kernel_size=(3,3), stride=(1,1), padding=(1,1))
+        self.decoder_conv6 = nn.Conv2d(128, 64, kernel_size=(3,3), stride=(1,1), padding=(1,1))
+        self.decoder_convt_4 = nn.ConvTranspose2d(64, 64, kernel_size=(3,3), stride=(2,2), padding=(0,0))
+        self.decoder_conv7 = nn.Conv2d(64, 64, kernel_size=(3,3), stride=(1,1), padding=(1,1))
+        self.decoder_conv8 = nn.Conv2d(64, 3, kernel_size=(3,3), stride=(1,1), padding=(1,1))
+        self.decoder_convt_5 = nn.ConvTranspose2d(3, 3, kernel_size=(2,2), stride=(1,1), padding=(0,0))
+        self.decoder_conv9 = nn.Conv2d(3, 3, kernel_size=(3,3), stride=(1,1), padding=(1,1))
+        self.decoder_conv10 = nn.Conv2d(3, 3, kernel_size=(3,3), stride=(1,1), padding=(1,1))
 
     
     def forward(self, x):
         x = self.vgg16_pretrained(x)
-        # x = self.prelu(self.decoder_conv1(x))
-        # x = self.decoder_convt_1(x)
+        x = self.prelu(self.decoder_conv1(x))
+        x = self.decoder_convt_1(x)
+        x = self.prelu(self.decoder_conv2(x))
+        x = self.decoder_convt_2(x)
+        x = self.prelu(self.decoder_conv3(x))
+        x = self.prelu(self.decoder_conv4(x))
+        x = self.decoder_convt_3(x)
+        x = self.prelu(self.decoder_conv5(x))
+        x = self.prelu(self.decoder_conv6(x))
+        x = self.decoder_convt_4(x)
+        x = self.prelu(self.decoder_conv7(x))
+        x = self.prelu(self.decoder_conv8(x))
+        x = self.decoder_convt_5(x)
+        x = self.prelu(self.decoder_conv9(x))
+        x = self.decoder_conv10(x)
         return x
