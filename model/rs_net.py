2b9dffe3d07cc99ca687450ee1a454b9719fb517
prajapatisarvesh <sarvesh101p@gmail.com>
2023-10-14 18:59:59 -0400

    minor-changes

--- model/rs_net.py
+++ model/rs_net.py
@@ -12,6 +12,12 @@ class RSNet(BaseModel):
             'features.1':'relu1_1'
         }
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        '''
+        ###############
+        Encoder Network
+        ###############
+        Using VGG-16 pretrained
+        '''
         self.vgg16_pretrained = torchvision.models.vgg16 (pretrained = True)
         '''
         Replace ReLU with PReLU
@@ -26,10 +32,30 @@ class RSNet(BaseModel):
         self.vgg16_pretrained.features[16] = nn.MaxPool2d(kernel_size=2, stride=1, padding=0, dilation=1, ceil_mode=False)
         self.vgg16_pretrained.features[30] = nn.MaxPool2d(kernel_size=2, stride=1, padding=0, dilation=1, ceil_mode=False)
         '''
-        Remove the classifier layer
+        Remove the classifier layer and avgpool layer
         '''
-        self.vgg16_pretrained.classifier = nn.Sequential(*list(self.vgg16_pretrained.classifier.children())[:-7])
+        self.vgg16_pretrained = nn.Sequential(*list(self.vgg16_pretrained.children())[:-2])
+        '''
+        add 1x1 conv
+        '''
+        idx = 31
+        self.vgg16_pretrained.add_module(f'{idx}', nn.Conv2d(512, 512, kernel_size=(1,1), padding=(0,0)))
+        idx+=1
+
+        '''
+        ###############
+        Decoder Network
+        ###############
+        Using model proposed by Zeiler
+        '''
+        self.prelu = nn.PReLU()
+        self.decoder_conv1 = nn.Conv2d(512, 512, kernel_size=(3,3), stride=(1,1), padding=(1,1))
+        self.decoder_convt_1 = nn.ConvTranspose2d(512, 512, kernel_size=(3,3), stride=(1,1), padding=(0,0), dilation=(3,3))
+
+
     
     def forward(self, x):
         x = self.vgg16_pretrained(x)
+        # x = self.prelu(self.decoder_conv1(x))
+        # x = self.decoder_convt_1(x)
         return x
